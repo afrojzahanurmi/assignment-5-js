71821b5ce4b20201710aec96db528bd300af4968
afrojzahanurmi <afrojzahanurme4560@gmail.com>
2024-09-24 22:02:48 +0600

    added the popup part

--- js/popup.js
+++ js/popup.js
@@ -9,3 +9,27 @@ document.getElementById('donate-btn-noa').addEventListener('click', function(){
     document.getElementById('popup-btn').addEventListener('click', function(){
         popup.classList.add("hidden")
         }) 
+
+    document.getElementById('feni-donate-btn').addEventListener('click', function(){
+        const popup2= document.getElementById('popup2');
+        popup2.classList.remove("hidden")
+        }) 
+
+        document.getElementById('popup-btn2').addEventListener('click', function(){
+            popup2.classList.add("hidden")
+            }) 
+
+
+        document.getElementById('Quota-donate-btn').addEventListener('click', function(){
+            const popup3= document.getElementById('popup3');
+            popup3.classList.remove("hidden")
+            }) 
+
+
+    document.getElementById('popup-btn3').addEventListener('click', function(){
+        popup3.classList.add("hidden")
+        }) 
+
+
+
+
